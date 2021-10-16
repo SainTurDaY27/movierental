bbe9b1674477312404b99296a416668981775c86
SainTurDaY27 <poom52344@gmail.com>
2021-10-16 16:15:09 +0700

    Finish refactor movierental.

--- rental.py
+++ rental.py
@@ -1,24 +1,31 @@
 class Rental:
-	"""
-	A rental of a movie by customer.
-	From Fowler's refactoring example.
+    """
+    A rental of a movie by customer.
+    From Fowler's refactoring example.
+    A realistic Rental would have fields for the dates
+    that the movie was rented and returned, from which the
+    rental period is calculated.
+    But for simplicity of the example only a days_rented
+    field is used.
+    """
 
-	A realistic Rental would have fields for the dates
-	that the movie was rented and returned, from which the
-	rental period is calculated.
-	But for simplicity of the example only a days_rented
-	field is used.
-	"""
-	
-	def __init__(self, movie, days_rented): 
-		"""Initialize a new movie rental object for
-		   a movie with known rental period (daysRented).
-		"""
-		self.movie = movie
-		self.days_rented = days_rented
+    def __init__(self, movie, days_rented):
+        """Initialize a new movie rental object for
+           a movie with known rental period (daysRented).
+        """
+        self.movie = movie
+        self.days_rented = days_rented
 
-	def get_movie(self):
-		return self.movie
+    def get_movie(self):
+        return self.movie
 
-	def get_days_rented(self):
-		return self.days_rented
+    def get_days_rented(self):
+        return self.days_rented
+
+    def get_price(self):
+        return self.get_movie().get_price_code().price(self.days_rented)
+
+    def get_renter_point(self):
+        frequent_renter_points = 0
+        frequent_renter_points += self.get_movie().get_price_code().frequent_renter_point(self.days_rented)
+        return frequent_renter_points
--- rental_test.py
+++ rental_test.py
@@ -1,30 +1,41 @@
 import unittest
 from customer import Customer
 from rental import Rental
-from movie import Movie
+from movie import *
 
 
 class RentalTest(unittest.TestCase):
-	
-	def setUp(self):
-		self.new_movie = Movie("Mulan", Movie.NEW_RELEASE)
-		self.regular_movie = Movie("CitizenFour", Movie.REGULAR)
-		self.childrens_movie = Movie("Frozen", Movie.CHILDRENS)
 
-	def test_movie_attributes(self):
-		"""trivial test to catch refactoring errors or change in API of Movie"""
-		m = Movie("CitizenFour", Movie.REGULAR)
-		self.assertEqual("CitizenFour", m.get_title())
-		self.assertEqual(Movie.REGULAR, m.get_price_code())
+    def setUp(self):
+        self.new_movie = Movie("Mulan", PriceCode.NEW_RELEASE)
+        self.regular_movie = Movie("CitizenFour", PriceCode.REGULAR)
+        self.childrens_movie = Movie("Frozen", PriceCode.CHILDRENS)
 
-	@unittest.skip("TODO add this test when you refactor rental price")
-	def test_rental_price(self):
-		rental = Rental(self.new_movie, 1)
-		self.assertEqual(rental.get_price(), 3.0)
-		rental = Rental(self.new_movie, 5)
-		self.assertEqual(rental.get_price(), 15.0)
-		self.fail("TODO add more tests for other movie categories")
+    def test_movie_attributes(self):
+        """trivial test to catch refactoring errors or change in API of Movie"""
+        m = Movie("CitizenFour", PriceCode.REGULAR)
+        self.assertEqual("CitizenFour", m.get_title())
+        self.assertEqual(PriceCode.REGULAR, m.get_price_code())
 
-	@unittest.skip("TODO add test of frequent renter points when you add it to Rental")
-	def test_rental_points(self):
-		self.fail("TODO add  test of frequent renter points")
+    def test_rental_price(self):
+        """Tests for method get_price() for returning rental price"""
+        rental = Rental(self.new_movie, 1)
+        self.assertEqual(rental.get_price(), 3.0)
+        rental = Rental(self.new_movie, 5)
+        self.assertEqual(rental.get_price(), 15.0)
+        rental = Rental(self.regular_movie, 7)
+        self.assertEqual(rental.get_price(), 9.5)
+        rental = Rental(self.childrens_movie, 4)
+        self.assertEqual(rental.get_price(), 3.0)
+
+    def test_rental_points(self):
+        """Tests for method get_renter_point() for returning rental point"""
+        rental = Rental(self.new_movie, 5)
+        self.assertEqual(rental.get_renter_point(), 5.0)
+        rental = Rental(self.new_movie, 7)
+        self.assertEqual(rental.get_renter_point(), 7.0)
+        rental = Rental(self.regular_movie, 6)
+        self.assertEqual(rental.get_renter_point(), 1.0)
+        rental = Rental(self.childrens_movie, 4)
+        self.assertEqual(rental.get_renter_point(), 1.0)
+
